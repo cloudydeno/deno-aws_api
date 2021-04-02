40ea7c8813475d5d24decd55579a4dcfbaeb208f
Daniel Lamando <dan@danopia.net>
2021-04-02 12:52:30 +0200

    deploy: Move S3 caching into other repo

--- generation/deploy/cache.ts
+++ generation/deploy/cache.ts
@@ -1,11 +1,12 @@
-import * as S3 from "https://deno.land/x/s3@0.4.0/mod.ts";
-import { s3Cache } from "./x-s3-cache.ts";
-import { inMemoryCache } from "https://deno.land/x/httpcache@0.1.2/in_memory.ts";
-import type { Cache } from "https://deno.land/x/httpcache@0.1.2/mod.ts";
+import { S3Bucket } from "https://deno.land/x/s3@0.4.0/mod.ts";
+import { s3Cache } from "https://raw.githubusercontent.com/cloudydeno/deno_httpcache/s3-cache/s3.ts";
+import { inMemoryCache } from "https://raw.githubusercontent.com/cloudydeno/deno_httpcache/s3-cache/in_memory.ts";
+import type { Cache } from "https://raw.githubusercontent.com/cloudydeno/deno_httpcache/s3-cache/mod.ts";
+// import type { Cache } from "https://deno.land/x/httpcache@0.1.2/mod.ts";
 
 // const s3Api = makeS3Client('us-east-2');
 // const s3Api = new S3(new ApiFactory());
-const s3Api = new S3.S3Bucket({
+const s3Api = new S3Bucket({
   accessKeyID: Deno.env.get('AWS_ACCESS_KEY_ID')!,
   secretKey: Deno.env.get('AWS_SECRET_ACCESS_KEY')!,
   sessionToken: Deno.env.get('AWS_SESSION_TOKEN'),
@@ -32,22 +33,20 @@ export async function immutableFetch(url: string) {
     }
   }
 
-  console.log('fetch', url);
   const resp = await fetch(url);
   if (resp.status === 200) {
     resp.headers.delete('expires');
-    resp.headers.set('cache-control', 'public, immutable');
+    resp.headers.set('cache-control', 'immutable');
   } else if (resp.status === 404) {
     resp.headers.delete('expires');
-    resp.headers.set('cache-control', 'public, maxage=3600');
+    resp.headers.set('cache-control', 'maxage=3600');
   } else {
-    resp.headers.set('cache-control', 'public, maxage=300');
+    resp.headers.set('cache-control', 'maxage=300');
   }
+  console.log('fetched', resp.status, url);
 
-  console.log('put', url, resp.status);
   await Promise.all(caches.map(cache => {
     cache.put(url, resp);
   }));
-
   return resp;
 }
