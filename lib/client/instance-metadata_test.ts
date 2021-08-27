6e998427d73b9f72b8fb5ada26958b7ae1fc8176
Daniel Lamando <me@danopia.net>
2021-08-27 18:42:19 +0200

    Update IMDS client to use AbortSignal (#12)
    * Update IMDS client to use AbortSignal
    
    Shipped in Deno v1.11 which is our minimum supported version.
    
    * Refactor IMDS client to use timeouts better
    
    * Re-enable IMDS test with working AbortSignal
    
    * Ignore another error in IMDS test (azure?)
    
    * Fix resource cleanup in IMDS error handler
    
    * Clear request timeout more reliably

--- lib/client/instance-metadata_test.ts
+++ lib/client/instance-metadata_test.ts
@@ -1,20 +1,17 @@
 import { IMDSv2 } from './instance-metadata.ts';
 
-Deno.test({
-  name: 'getting a cached metadata token',
-  async fn() {
-    return; // TODO! re-enable test once AbortSignal is used properly
-    const imds = new IMDSv2();
-    try {
-      const token = await imds.getToken();
-      if (token !== await imds.getToken()) {
-        throw new Error(`IMDSv2 token changed; should've been cached`);
-      }
-    } catch (err) {
-      const {message} = err as Error;
-      if (!(typeof message === 'string')) throw err;
-      if (!message.includes('Instance Metadata Timeout')) throw err;
+Deno.test('getting a cached metadata token', async () => {
+  const imds = new IMDSv2();
+  try {
+    const token = await imds.getToken();
+    if (token !== await imds.getToken()) {
+      throw new Error(`IMDSv2 token changed; should've been cached`);
     }
-  },
-  sanitizeOps: false, // We leak the fetch until Deno adds fetch Aborting
+  } catch (err) {
+    const {message} = err as Error;
+    if (!(typeof message === 'string')) throw err;
+    if (message.includes('Instance Metadata Timeout')) return;
+    if (message.includes('is this not AWS?')) return;
+    throw err;
+  }
 });
