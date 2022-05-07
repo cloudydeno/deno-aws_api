d209782715d1c67e2ef67b232ebc5cb960ba11e4
Daniel Lamando <dan@danopia.net>
2022-05-07 14:09:36 +0200

    Somewhat better segmenter tests

--- lib/helpers/s3-upload_test.ts
+++ lib/helpers/s3-upload_test.ts
@@ -1,19 +1,45 @@
-import { newPartSegmenter } from "./s3-upload.ts";
 import { just } from "https://deno.land/x/stream_observables@v1.2/sources/just.ts";
 import { collect } from "https://deno.land/x/stream_observables@v1.2/sinks/collect.ts";
+import { assertEquals } from "https://deno.land/std@0.130.0/testing/asserts.ts";
 
-Deno.test('newPartSegmenter', async () => {
-  const parts = await collect(just('1234567890')
+import { newPartSegmenter } from "./s3-upload.ts";
+
+Deno.test('newPartSegmenter', async (t) => {
+
+  const segmentStrings = (opts: {
+    maxSize: number,
+    inputChunks: string[],
+  }) => collect(just(...opts.inputChunks)
     .pipeThrough(new TextEncoderStream())
-    .pipeThrough(newPartSegmenter(8)));
+    .pipeThrough(newPartSegmenter(opts.maxSize))
+    .pipeThrough(new TextDecoderStream()));
 
-  console.log(parts);
+  await t.step('cuts one segment up', async () => {
+    assertEquals(await segmentStrings({
+      maxSize: 8,
+      inputChunks: ['1234567890'],
+    }), [
+      '12345678',
+      '90',
+    ]);
+  });
+
+  await t.step('recombines small segments', async () => {
+    assertEquals(await segmentStrings({
+      maxSize: 8,
+      inputChunks: ['12', '34', '56', '78', '90'],
+    }), [
+      '12345678',
+      '90',
+    ]);
+  });
+
+  await t.step('handles exact fits', async () => {
+    assertEquals(await segmentStrings({
+      maxSize: 5,
+      inputChunks: ['12345'],
+    }), [
+      '12345',
+    ]);
+  });
 });
-
-Deno.test('newPartSegmenter', async () => {
-  const parts = await collect(just('12', '34', '56', '78', '90')
-    .pipeThrough(new TextEncoderStream())
-    .pipeThrough(newPartSegmenter(8)));
-
-  console.log(parts);
-})
