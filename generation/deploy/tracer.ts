29464e33b9c5f43c596d2b19a5e7ea152a2b35d5
Daniel Lamando <dan@danopia.net>
2023-03-14 01:27:12 +0100

    Update tracer again

--- generation/deploy/tracer.ts
+++ generation/deploy/tracer.ts
@@ -1,14 +1,5 @@
-import {
-  DenoTracerProvider,
-  OTLPTraceFetchExporter,
-  httpTracer,
-  DenoFetchInstrumentation,
-  Resource,
-  trace,
-} from "https://raw.githubusercontent.com/cloudydeno/deno-observability/9d996d1ce0ba6b15641cbc882b15fc63992418b9/tracing/mod.ts";
-import { GcpBatchSpanExporter } from "https://raw.githubusercontent.com/cloudydeno/deno-observability/9d996d1ce0ba6b15641cbc882b15fc63992418b9/tracing/exporters/google-cloud.ts";
-import { GoogleCloudPropagator } from "https://raw.githubusercontent.com/cloudydeno/deno-observability/9d996d1ce0ba6b15641cbc882b15fc63992418b9/tracing/propagators/google-cloud.ts";
-
+import { httpTracer, trace, DenoFetchInstrumentation, DenoTracerProvider, OTLPTraceFetchExporter, Resource } from "https://deno.land/x/observability@v0.2.0/tracing/mod.ts";
+import { GoogleCloudPropagator } from "https://deno.land/x/observability@v0.2.0/tracing/propagators/google-cloud.ts";
 export { httpTracer, trace };
 
 export const provider = new DenoTracerProvider({
@@ -23,7 +14,6 @@ export const provider = new DenoTracerProvider({
     new DenoFetchInstrumentation(),
   ],
   batchSpanProcessors: [
-    new GcpBatchSpanExporter(),
-    // new OTLPTraceFetchExporter(),
+    new OTLPTraceFetchExporter(),
   ],
 });
