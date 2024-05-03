d7395860a65862b11e6d6c860a94da0b5bfca599
Daniel Lamando <dan@danopia.net>
2024-05-03 12:53:09 +0200

    Update tracer to fix missing root span

--- generation/deploy/tracer.ts
+++ generation/deploy/tracer.ts
@@ -1,7 +1,7 @@
-export { httpTracer } from "https://deno.land/x/observability@v0.5.1/instrumentation/http-server.ts";
-export { trace, SpanKind } from "https://deno.land/x/observability@v0.5.1/opentelemetry/api.js";
+export { httpTracer } from "https://deno.land/x/observability@v0.5.3/instrumentation/http-server.ts";
+export { trace, SpanKind } from "https://deno.land/x/observability@v0.5.3/opentelemetry/api.js";
 
-import { DenoTelemetrySdk } from "https://deno.land/x/observability@v0.5.1/sdk.ts";
+import { DenoTelemetrySdk } from "https://deno.land/x/observability@v0.5.3/sdk.ts";
 
 new DenoTelemetrySdk({
   resourceAttrs: {
