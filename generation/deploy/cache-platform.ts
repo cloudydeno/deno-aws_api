cede9878917bbb78146b74094c131663e2517937
Daniel Lamando <dan@danopia.net>
2024-09-23 22:40:53 +0200

    Add Deno platform cache to codegen service (#47)
    * Add Deno platform cache to codegen service
    
    * register cache differently?
    
    * Another attempt at edge caching
    
    * Redo platform cache fetch
    
    * add debug event
    
    * Hmm so we need to do cache versioning?
    
    * Add tracing event for cache hit failure
    
    * Ok, maybe need to x- the header
    
    * Remove debug tracing event

---
+++ generation/deploy/cache-platform.ts
@@ -0,0 +1,65 @@
+import { Cache } from "https://deno.land/x/httpcache@0.1.2/mod.ts";
+import { trace } from "./tracer.ts";
+
+// https://docs.deno.com/deploy/manual/edge-cache
+
+function transformUrl(url: string) {
+  return url.replace(/\/\/[^\/]+\//, '//upstream-cache-v2/');
+}
+
+export async function platformCache(
+  name = 'http-policy-cache',
+): Promise<Cache> {
+
+  const nativeCache = await caches.open(name);
+
+  return new Cache({
+
+    async get(url) {
+      const cached = await nativeCache.match(transformUrl(url));
+      if (!cached) return undefined;
+
+      type CachePolicy = Parameters<ConstructorParameters<typeof Cache>[0]['set']>[1]['policy'];
+
+      const {
+        'x-cache-policy': policyHeader,
+        ...resh
+      } = Object.fromEntries(cached.headers.entries());
+
+      if (!policyHeader) {
+        trace.getActiveSpan()?.addEvent('edgecache.fail', {
+          'message': 'platform cache hit lacked policy',
+        });
+        console.warn('WARN: platform cache lacked policy', cached);
+        return undefined;
+      }
+      const policy = {
+        ...JSON.parse(policyHeader),
+        resh,
+      } as CachePolicy;
+
+      const BodyBuffer = new Uint8Array(await cached.arrayBuffer());
+
+      return {
+        policy,
+        body: BodyBuffer,
+      };
+    },
+
+    async set(url, resp) {
+      const {resh, ...rest} = resp.policy;
+      await nativeCache.put(transformUrl(url), new Response(resp.body, {
+        headers: {
+          ...resh,
+          'x-cache-policy': JSON.stringify(rest),
+        },
+      }));
+    },
+
+    async delete(url) {
+      await nativeCache.delete(transformUrl(url));
+    },
+
+    close() {},
+  });
+}
