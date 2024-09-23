f33650835a1588349d44ce75f91749026d0fbf55
Daniel Lamando <dan@danopia.net>
2024-09-23 22:04:40 +0200

    Hmm so we need to do cache versioning?

--- generation/deploy/cache-platform.ts
+++ generation/deploy/cache-platform.ts
@@ -3,6 +3,10 @@ import { trace } from "./tracer.ts";
 
 // https://docs.deno.com/deploy/manual/edge-cache
 
+function transformUrl(url: string) {
+  return url.replace(/\/\/[^\/]+\//, '//upstream-cache-v1/');
+}
+
 export async function platformCache(
   name = 'http-policy-cache',
 ): Promise<Cache> {
@@ -12,7 +16,7 @@ export async function platformCache(
   return new Cache({
 
     async get(url) {
-      const cached = await nativeCache.match(url);
+      const cached = await nativeCache.match(transformUrl(url));
 
       trace.getActiveSpan()?.addEvent('edgecache', {
         'cache.result': cached ? 'hit' : 'miss',
@@ -46,7 +50,7 @@ export async function platformCache(
 
     async set(url, resp) {
       const {resh, ...rest} = resp.policy;
-      await nativeCache.put(url, new Response(resp.body, {
+      await nativeCache.put(transformUrl(url), new Response(resp.body, {
         headers: {
           ...resh,
           'cache-policy': JSON.stringify(rest),
@@ -55,7 +59,7 @@ export async function platformCache(
     },
 
     async delete(url) {
-      await nativeCache.delete(url);
+      await nativeCache.delete(transformUrl(url));
     },
 
     close() {},
